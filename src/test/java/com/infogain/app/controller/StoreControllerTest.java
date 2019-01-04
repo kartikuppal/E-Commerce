1118962284c65f507de9f80db24c4dd518d480ec
singhharshit6894 <singhharshit6894@gmail.com>
2019-01-04 14:56:45 +0530

    files updated with test cases

---
+++ src/test/java/com/infogain/app/controller/StoreControllerTest.java
@@ -0,0 +1,204 @@
+package com.infogain.app.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.infogain.app.dto.StoreDto;
+import com.infogain.app.service.StoreServiceImpl;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(controllers = StoreController.class, secure = false)
+public class StoreControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private StoreServiceImpl storeService;
+	
+	private String mapToJson(Object object) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(object);
+	}
+	
+	@Test
+	public void create() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(1);
+		storeDto.setName("anil");
+		storeDto.setAddress("amritsar india");
+		storeDto.setPostalCode("123654");
+		storeDto.setContactNo("7896541236");
+		
+		String inputInJson = this.mapToJson(storeDto);
+		
+		Mockito.when(storeService.insert(Mockito.any(StoreDto.class))).thenReturn(storeDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.post("/api/store")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(inputInJson)
+				.contentType(MediaType.APPLICATION_JSON)).andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		assertThat(outputInJson).isEqualTo(inputInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	@Test
+	public void create2() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(null);
+		storeDto.setName(null);
+		storeDto.setAddress(null);
+		storeDto.setPostalCode(null);
+		storeDto.setContactNo(null);
+		
+		String inputInJson = this.mapToJson(storeDto);
+		
+		Mockito.when(storeService.insert(Mockito.any(StoreDto.class))).thenReturn(storeDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.post("/api/store")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(inputInJson)
+				.contentType(MediaType.APPLICATION_JSON)).andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		assertThat(outputInJson).isEqualTo(inputInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	@Test
+	public void create3() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(1);
+		storeDto.setName("ansdgfweil");
+		storeDto.setAddress("ia");
+		storeDto.setPostalCode("1dfsg23654");
+		storeDto.setContactNo("78965sdfgfsdgfsdgfd");
+		
+		String inputInJson = this.mapToJson(storeDto);
+		
+		Mockito.when(storeService.insert(Mockito.any(StoreDto.class))).thenReturn(storeDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.post("/api/store")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(inputInJson)
+				.contentType(MediaType.APPLICATION_JSON)).andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		assertThat(outputInJson).isEqualTo(inputInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	
+	/*display*/
+	
+	@Test
+	public void getAll() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(1);
+		storeDto.setName("anil");
+		storeDto.setAddress("amritsar india");
+		storeDto.setPostalCode("123654");
+		storeDto.setContactNo("7896541236");
+		
+		List<StoreDto> storeDtoList = new ArrayList<>();
+		storeDtoList.add(storeDto);
+		
+		String expectedJson = this.mapToJson(storeDtoList);
+		
+		Mockito.when(storeService.displayAll()).thenReturn(storeDtoList);
+		
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders
+						.get("/api/store")
+						.accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+		
+		String outputInJson = result.getResponse().getContentAsString();
+		assertThat(outputInJson).isEqualTo(expectedJson);
+	}
+	
+	/*display by id*/
+	
+	@Test
+	public void getById() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(1);
+		storeDto.setName("ansdgfweil");
+		storeDto.setAddress("ia");
+		storeDto.setPostalCode("1dfsg23654");
+		storeDto.setContactNo("78965sdfgfsdgfsdgfd");
+		
+		Mockito.when(storeService.displayById(Mockito.anyInt())).thenReturn(storeDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.get("/api/store/1")
+				.accept(MediaType.APPLICATION_JSON)).andReturn();
+		
+		String expectedJson = this.mapToJson(storeDto);
+		String outputInJson = result.getResponse().getContentAsString();
+		assertThat(outputInJson).isEqualTo(expectedJson);
+	}
+	@Test
+	public void getById2() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(1);
+		storeDto.setName("ansdgfweil");
+		storeDto.setAddress("ia");
+		storeDto.setPostalCode("1dfsg23654");
+		storeDto.setContactNo("78965sdfgfsdgfsdgfd");
+		
+		Mockito.when(storeService.displayById(Mockito.anyInt())).thenReturn(storeDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.get("/api/store/a")
+				.accept(MediaType.APPLICATION_JSON)).andReturn();
+		
+		String expectedJson = this.mapToJson(storeDto);
+		String outputInJson = result.getResponse().getContentAsString();
+		assertThat(outputInJson).isEqualTo(expectedJson);
+	}
+	@Test
+	public void getById3() throws Exception {
+		StoreDto storeDto = new StoreDto();
+		storeDto.setId(1);
+		storeDto.setName("ansdgfweil");
+		storeDto.setAddress("ia");
+		storeDto.setPostalCode("1dfsg23654");
+		storeDto.setContactNo("78965sdfgfsdgfsdgfd");
+		
+		Mockito.when(storeService.displayById(Mockito.anyInt())).thenReturn(storeDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.get("/api/store/")
+				.accept(MediaType.APPLICATION_JSON)).andReturn();
+		
+		String expectedJson = this.mapToJson(storeDto);
+		String outputInJson = result.getResponse().getContentAsString();
+		assertThat(outputInJson).isEqualTo(expectedJson);
+	}
+}
--- src/test/java/com/infogain/app/controller/UserControllerTest.java
+++ src/test/java/com/infogain/app/controller/UserControllerTest.java
@@ -2,6 +2,10 @@ package com.infogain.app.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -14,79 +18,162 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infogain.app.dto.UserDto;
+import com.infogain.app.exception.InvalidInputException;
 import com.infogain.app.service.UserServiceImpl;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = UserController.class, secure = false)
 public class UserControllerTest {
-
 	@Autowired
-	private MockMvc mvc;
-
+	private MockMvc mockMvc;
 	@MockBean
 	private UserServiceImpl userService;
-
+	
 	@Test
-	public void testGetAll() {
+	public void create() throws Exception {
 		UserDto userDto = new UserDto();
 		userDto.setId(1);
-		userDto.setAddress("address hhhhhh");
-		userDto.setEmail("email");
-		userDto.setMobileNumber("9874335512");
-		userDto.setStatus((byte) 1);
-		userDto.setPostalCode("143001");
-		userDto.setPassword("user1234");
-
-	}
-
-	@Test
-	public void testGetById() throws Exception {
-		UserDto userDto = new UserDto();
-		userDto.setId(1);
-		userDto.setName("harshit");
-		userDto.setEmail("ejgu");
-		userDto.setAddress("sxdhcgdhc");
-		userDto.setMobileNumber("15787635487");
-		userDto.setPostalCode("57841254");
-
-		Mockito.when(userService.getById(Mockito.anyInt())).thenReturn(userDto);
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("api/user/1").accept(MediaType.APPLICATION_JSON);
+		userDto.setName("anil");
+		userDto.setEmail("anil@gmail.com");
+		userDto.setPassword("sdf1sfsdf2");
+		userDto.setAddress("noida india");
+		userDto.setMobileNumber("1236547895");
+		userDto.setPostalCode("123654");
+		userDto.setStatus((byte) 0);
 		
-		MvcResult result = mvc.perform(requestBuilder).andReturn();
+		
+		String inputInJson = this.mapToJson(userDto);
+		
+		Mockito.when(userService.insert(Mockito.any(UserDto.class))).thenReturn(userDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.post("/api/user")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(inputInJson)
+				.contentType(MediaType.APPLICATION_JSON)).andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		assertThat(outputInJson).isEqualTo(inputInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	@Test
+	public void create2() throws Exception {
+		UserDto userDto = new UserDto();
+		userDto.setId(null);
+		userDto.setName(null);
+		userDto.setEmail(null);
+		userDto.setPassword(null);
+		userDto.setAddress(null);
+		userDto.setMobileNumber(null);
+		userDto.setPostalCode(null);
+		userDto.setStatus(null);
+		
+		
+		String inputInJson = this.mapToJson(userDto);
+		
+		Mockito.when(userService.insert(Mockito.any(UserDto.class))).thenReturn(userDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.post("/api/user")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(inputInJson)
+				.contentType(MediaType.APPLICATION_JSON)).andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		assertThat(outputInJson).isEqualTo(inputInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	@Test
+	public void create3() throws Exception {
+		UserDto userDto = new UserDto();
+		userDto.setId(1);
+		userDto.setName("anil");
+		userDto.setEmail("anim");
+		userDto.setPassword("hj");
+		userDto.setAddress("noida india");
+		userDto.setMobileNumber("1236yu547895");
+		userDto.setPostalCode("1236yu54");
+		userDto.setStatus((byte) 0);
+		
+		
+		String inputInJson = this.mapToJson(userDto);
+		
+		Mockito.when(userService.insert(Mockito.any(UserDto.class))).thenReturn(userDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.post("/api/user")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(inputInJson)
+				.contentType(MediaType.APPLICATION_JSON)).andReturn();
+		
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		assertThat(outputInJson).isEqualTo(inputInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+	}
+	
+	@Test
+	public void getAll() throws Exception {
+		UserDto userDto = new UserDto();
+		userDto.setId(1);
+		userDto.setName("anil");
+		userDto.setEmail("anil@gmail.com");
+		userDto.setPassword("sdf1sfsdf2");
+		userDto.setAddress("noida india");
+		userDto.setMobileNumber("1236547895");
+		userDto.setPostalCode("123654");
+		userDto.setStatus((byte) 0);
+		
+		List<UserDto> userList = new ArrayList<>();
+		userList.add(userDto);
+		
+		String expectedJson = this.mapToJson(userList);
+		
+		Mockito.when(userService.getAll()).thenReturn(userList);
+		
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders
+						.get("/api/user")
+						.accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+		
+		String outputInJson = result.getResponse().getContentAsString();
+		assertThat(outputInJson).isEqualTo(expectedJson);
+	}
+	
+	@Test
+	public void getById() throws Exception {
+		UserDto userDto = new UserDto();
+		userDto.setId(1);
+		userDto.setName("anil");
+		userDto.setEmail("anil@gmail.com");
+		userDto.setPassword("sdf1sfsdf2");
+		userDto.setAddress("noida india");
+		userDto.setMobileNumber("1236547895");
+		userDto.setPostalCode("123654");
+		userDto.setStatus((byte) 0);
+		
+		Mockito.when(userService.getById(Mockito.anyInt())).thenReturn(userDto);
+
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.get("/api/user/1")
+				.accept(MediaType.APPLICATION_JSON)).andReturn();
+		
 		String expectedJson = this.mapToJson(userDto);
 		String outputInJson = result.getResponse().getContentAsString();
 		assertThat(outputInJson).isEqualTo(expectedJson);
 	}
-
-	@Test
-	public void testCreate() throws Exception {
-		UserDto userDto = new UserDto();
-		userDto.setId(1);
-		userDto.setName("User");
-		userDto.setAddress("address hhhhhh");
-		userDto.setEmail("emai5@mail.com");
-		userDto.setMobileNumber("9974696969");
-		userDto.setStatus((byte) 1);
-		userDto.setPostalCode("143001");
-		userDto.setPassword("user1234");
-		String inputInJson = this.mapToJson(userDto);
-		Mockito.when(userService.insert(Mockito.any(UserDto.class))).thenReturn(userDto);
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/user").accept(MediaType.APPLICATION_JSON)
-										.content(inputInJson).contentType(MediaType.APPLICATION_JSON);
-
-		MvcResult result = mvc.perform(requestBuilder).andReturn();
-		MockHttpServletResponse response = result.getResponse();
-
-		String outputInJson = response.getContentAsString();
-
-		assertThat(outputInJson).isEqualTo(inputInJson);
-		assertEquals(HttpStatus.OK.value(), response.getStatus());
-	}
+	
+	
 
 	private String mapToJson(Object object) throws JsonProcessingException {
 		ObjectMapper objectMapper = new ObjectMapper();
